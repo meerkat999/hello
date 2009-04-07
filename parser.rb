ea2c14202b3e264cb3c859b568a690f360b5052e
Dan Croak <dcroak@thoughtbot.com>
2009-04-07 00:14:50 -0400

    parser has done its parsing

---
+++ parser.rb
@@ -0,0 +1,50 @@
+module Hello
+  KINDS = %w(male female morning afternoon evening agnostic)
+end
+
+module Parser
+  def self.parse(input)
+    each_line(input) do |line|
+      Hello::KINDS.each { |kind| append(line, kind) }
+    end
+  end
+
+  def self.append(line, kind)
+    File.open(kind, "a") do |file|
+      file.write(line.hello) if line.send(:"#{kind}?")
+    end
+  end
+
+  def self.each_line(input)
+    IO.foreach(input) { |each| yield Line.new(each) }
+  end
+end
+
+module Cleaner
+  def self.clean
+    Hello::KINDS.each { |each| system "rm #{each}" }
+  end
+end
+
+class Line
+  attr_accessor :hello, :about
+
+  def initialize(input)
+    @about = input[0..44]
+    @hello = input[45..input.length].lstrip.rstrip << "\n"
+  end
+
+  Hello::KINDS.each do |kind|
+    define_method(:"#{kind}?") do
+      about.include? kind
+    end
+  end
+
+  def agnostic?
+    Hello::KINDS.all? { |kind| ! about.include?(kind) }
+  end
+end
+
+Cleaner.clean
+Parser.parse "all"
+
