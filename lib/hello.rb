e9765d795795a6d590fc2246df9d476bcef77e5b
Dan Croak <dcroak@thoughtbot.com>
2009-04-07 21:23:53 -0400

    refactoring implementation to be reasonable, removed all metaprogramming experiments

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,32 +2,17 @@ class Hello
   DATA_PATH = File.join(File.dirname(__FILE__), "..", "data")
 
   def self.random(opts = {})
-    if opts.key?(:gender)
-      random_gender_hello(opts[:gender])
-    elsif opts.key?(:time)
-      random_time_hello(opts[:time])
-    elsif opts.keys.any?
-      invalid_option_feedback
-    else
-      random_agnostic_hello
-    end
+    invalid_option_feedback unless valid_options?(opts)
+    hellos = collect_hellos("agnostic")
+    hellos |= collect_hellos("male")      if male?(opts)
+    hellos |= collect_hellos("female")    if female?(opts)
+    hellos |= collect_hellos("morning")   if morning?(opts)
+    hellos |= collect_hellos("afternoon") if afternoon?(opts)
+    hellos |= collect_hellos("evening")   if evening?(opts)
+    hellos[rand(hellos.size)]
   end
 
   protected
-
-  def self.method_missing(name, *args, &block)
-    if name.to_s =~ /^random_(.*)_hello$/
-      if args.any?
-        data = args.first.to_s
-      else
-        data = $1
-      end
-      hellos = collect_hellos(data)
-      hellos[rand(hellos.size)]
-    else
-      super
-    end
-  end
 
   def self.collect_hellos(kind)
     File.open(File.join(DATA_PATH, kind)) do |file|
@@ -35,7 +20,33 @@ class Hello
     end
   end
 
+  def self.male?(opts)
+    opts.key?(:gender) && opts[:gender] == :male
+  end
+
+  def self.female?(opts)
+    opts.key?(:gender) && opts[:gender] == :female
+  end
+
+  def self.morning?(opts)
+    opts.key?(:time) && opts[:time] == :morning
+  end
+
+  def self.afternoon?(opts)
+    opts.key?(:time) && opts[:time] == :afternoon
+  end
+
+  def self.evening?(opts)
+    opts.key?(:time) && opts[:time] == :evening
+  end
+
   def self.invalid_option_feedback
     raise StandardError, "see the README for valid options"
+  end
+
+  def self.valid_options?(opts)
+    opts.keys.empty? ||
+      male?(opts) || female?(opts) ||
+      morning?(opts) || afternoon?(opts) || evening?(opts)
   end
 end
