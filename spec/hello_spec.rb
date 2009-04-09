5ba3802dea0869c002e4a86719c6e9cd7d2477bf
Dan Croak <dcroak@thoughtbot.com>
2009-04-08 20:40:33 -0400

    adding spec for multiple options

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -113,6 +113,26 @@ describe "random with evening time" do
   end
 end
 
+describe "random with time and gender" do
+  before { @hello = Hello.random(:time => :morning, :gender => :female) }
+
+  it "should not be blank" do
+    @hello.should_not be_blank
+  end
+
+  it "should be a hello from morning, female, or inoffensive" do
+    hellos = File.hello_data("morning") & File.hello_data("female") 
+    hellos &= File.hello_data("inoffensive").to_a
+    hellos.include? @hello
+  end
+
+  %w(male morning afternoon evening).each do |kind|
+    it "should not be a hello from #{kind}" do
+      ! File.hello_data(kind).include? @hello
+    end
+  end
+end
+
 describe "an invalid option" do
   it "should provide feedback for invalid option" do
     lambda { Hello.random(:does_not => :exist) }.should raise_error
